5987d500767e953bf86ac0282e58eea23469e97c
pgerstoft <philipgerstoft@gmail.com>
2017-07-13 14:08:13 -0700

    HelloWorld!

---
+++ src/main/java/slack/HelloWorld.java
@@ -0,0 +1,33 @@
+package slack;
+
+import com.google.appengine.api.utils.SystemProperty;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Properties;
+
+@WebServlet(name = "HelloAppEngine", value = "/hello")
+public class HelloWorld extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+
+        Properties properties = System.getProperties();
+
+        response.setContentType("text/plain");
+        response.getWriter().println("Hello App Engine - Standard using "
+                + SystemProperty.version.get() + " Java " + properties.get("java.specification.version"));
+
+    }
+
+    public static String getInfo() {
+        return "Version: " + System.getProperty("java.version")
+                + " OS: " + System.getProperty("os.name")
+                + " User: " + System.getProperty("user.name");
+    }
+
+}
